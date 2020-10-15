d075e16bff72e991fcea5f284e0cd1d758f9fc1e
Jarrod Hatting <jarrodhatting@Jarrods-MacBook-Pro-2.local>
2020-10-15 13:37:50 -0700

    under construction lable

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h2>This Website is currently</h2>
+        <h1>UNDER CONSTRUCTION!</h1>
       </header>
     </div>
   );
