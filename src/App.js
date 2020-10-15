e8ff7801e5959a586b559e6a941d3e05861499b3
Jarrod Hatting <jarrodhatting@Jarrods-MacBook-Pro-2.local>
2020-10-15 13:59:45 -0700

    rot logo added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import rotlogo from './rotlogo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={rotlogo} className="App-logo" alt="logo" />
         <h2>This Website is currently</h2>
         <h1>UNDER CONSTRUCTION!</h1>
       </header>
